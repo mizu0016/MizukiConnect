00ae7dd556d73395558cac3fe8878f870b97567e
mizu0016 <mizu.nagnag@gmail.com>
2026-03-05 17:08:42 +0900

    Add MainActivity class for app entry point

---
+++ app/src/main/java/com/mizuki/connect/MainActivity.kt
@@ -0,0 +1,10 @@
+package com.mizuki.connect
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
+
